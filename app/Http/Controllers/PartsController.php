3cd226d7089c2395206624cbcd34192cd2842d22
Skrudj <skrudjmcduck1010@gmail.com>
2026-09-17 18:46:54 +0300

    Refresh cached product payloads with previews

--- app/Http/Controllers/PartsController.php
+++ app/Http/Controllers/PartsController.php
@@ -200,7 +200,7 @@ class PartsController extends Controller
 
         try {
             $productData = $this->cachedStorefrontPayload(
-                'storefront:product:v3:'.$locale.':'.$product,
+                'storefront:product:v4:'.$locale.':'.$product,
                 fn (): Response => $client->product($product, $locale),
                 [404, 410, 422],
             );
