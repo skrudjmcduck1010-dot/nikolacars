791b164904cdb8a3e76ae3f8426e00d203f23a1c
Skrudj <skrudjmcduck1010@gmail.com>
2026-09-18 12:57:16 +0300

    Return correct status for missing catalog sections

--- app/Http/Controllers/PartsController.php
+++ app/Http/Controllers/PartsController.php
@@ -81,8 +81,9 @@ class PartsController extends Controller
 
         try {
             $initialCatalog = $this->cachedStorefrontPayload(
-                'storefront:catalog:v2:'.sha1(json_encode($catalogQuery, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)),
+                'storefront:catalog:v3:'.sha1(json_encode($catalogQuery, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)),
                 fn (): Response => $client->catalog($catalogQuery),
+                [404, 410, 422],
             );
             $initialCatalog = $this->withProductUrlSlugs($initialCatalog);
         } catch (ConnectionException|RuntimeException $exception) {
